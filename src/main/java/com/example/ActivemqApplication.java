13f3fda6eace2040b1d06d22d26f805e63e9a596
lize <243620572>
2018-07-21 16:51:56 +0800

    1

--- src/main/java/com/example/ActivemqApplication.java
+++ src/main/java/com/example/ActivemqApplication.java
@@ -1,5 +1,6 @@
 package com.example;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +9,6 @@ public class ActivemqApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ActivemqApplication.class, args);
+		System.out.print("启动成功");
 	}
 }
